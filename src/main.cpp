73e65a9d3714bb87061ac5024b24df367ad29bda
makipoea <makipoea@gmail.com>
2025-09-07 17:23:06 +0200

    a small start

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include <igl/read_triangle_mesh.h>
+#include <Eigen/Core>
+
+
+//using namespace std;
+
+int main() {
+
+    Eigen::MatrixXd V; // sommets
+    Eigen::MatrixXi F; // faces
+
+
+    std::string filename = "../resources/lens.stl";
+    if (!igl::read_triangle_mesh(filename, V, F)) {
+        std::cerr << "Erreur : impossible de charger " << filename << std::endl;
+        return -1;
+    }
+
+
+    std::cout << "Maillage chargé : " << filename << std::endl;
+    std::cout << "Nombre de sommets : " << V.rows() << std::endl;
+    std::cout << "Nombre de faces   : " << F.rows() << std::endl;
+
+    if (V.rows() > 0) {
+        std::cout << "Premier sommet : " 
+                  << V(0,0) << ", " 
+                  << V(0,1) << ", " 
+                  << V(0,2) << std::endl;
+    }
+
+    return 0;
+}
---
+++ src/simulation/Object/Component.cpp
@@ -0,0 +1,4 @@
+#include "Component.hpp"
+#include<iostream>
+
+
